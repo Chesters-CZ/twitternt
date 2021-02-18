9526c596312b632b855572a83704f68c02202222
Chesters-CZ <bezouskavit@gmail.com>
2021-02-18 13:32:16 +0100

    předělání základu

--- backend/src/main/java/cz/educanet/zolololgicka/ejpiajkonfig.java
+++
@@ -1,8 +0,0 @@
-package cz.educanet.zolololgicka;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("zolo")
-public class ejpiajkonfig extends Application {
-}

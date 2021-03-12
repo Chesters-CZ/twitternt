24d34b18679ace0c29e4df73673368372d13fbab
Chesters-CZ <bezouskavit@gmail.com>
2021-03-12 12:54:29 +0100

    práce i pro dava

--- backend/src/main/webapp/java/cz/educanet/twitternt/Tweets.java
+++ backend/src/main/webapp/java/cz/educanet/twitternt/Tweets.java
@@ -16,7 +16,7 @@ public class Tweets {
         return Response.ok(TweetDatabase.tweetsList.toString()).build();
     }
 
-    //TODO: get tweets od jednoho uživatele, pass + uname input a response budou buď všechny tweety od uživatele nebo 404
+    //TODO: Malik - get tweets od jednoho uživatele, pass + uname input a response budou buď všechny tweety od uživatele nebo 404
 
     @POST
     public Response PostTweet(@QueryParam("user") String username, @QueryParam("pass") String password, @QueryParam("head") String header, @QueryParam("body") String body) {
@@ -34,16 +34,17 @@ public class Tweets {
     }
 
     @DELETE
-    public Response RemoveTweet(@QueryParam("user") String username, @QueryParam("pass") String password,  @QueryParam("head") String header, @QueryParam("body") String body) {
+    public Response RemoveTweet(@QueryParam("user") String username, @QueryParam("pass") String password, @QueryParam("head") String header, @QueryParam("body") String body) {
         for (int i = 0; i < TweetDatabase.tweetsList.size(); i++) {
             if (TweetDatabase.tweetsList.get(i).header.equals(header) && TweetDatabase.tweetsList.get(i).body.equals(body)) {
-                    if (TweetDatabase.tweetsList.get(i).author.equals(new User(username, password))){
-                        TweetDatabase.tweetsList.remove(i);
-                        return Response.ok("removed post " + header).build();
-                    }
-                    else return Response.status(401, "supplied credentials do not match author credentials.").build();
+                if (TweetDatabase.tweetsList.get(i).author.equals(new User(username, password))) {
+                    TweetDatabase.tweetsList.remove(i);
+                    return Response.ok("removed post " + header).build();
+                } else return Response.status(401, "supplied credentials do not match author credentials.").build();
             }
         }
         return Response.status(404, "This post does not exist.").build();
     }
+
+    //TODO: David - přidat liketweet metodu, asiže put, nejlíp tak, aby se kontrolovalo kdo likuje, tzn. max. 1 like/uživatel. klidně uprav i tweet classu a udělej něco jako .likes a .likedBy
 }
